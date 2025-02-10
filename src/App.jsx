36ce87f30b86e952e171e909a3dbaa68d197627d
RahulAnand-GitHub <rahul.anand88096@gmail.com>
2025-02-10 23:40:49 +0530

    Added Routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Body from './components/Body'
+import Login from './components/Login'
+import Profile from './components/Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>Hello World</div>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
